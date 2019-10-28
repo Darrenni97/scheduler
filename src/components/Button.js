d7ddd9dcf852867bfdc7c0bd7ab336759f714c0c
Darrenni97 <darrenni_@hotmail.com>
2019-10-28 16:16:46 -0700

    added dayList to navbar

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,21 +2,21 @@ import React from "react";
 
 import "components/Button.scss";
 
-const className = require('classnames');
+const className = require("classnames");
 
 export default function Button(props) {
-   const buttonClass = className("button", {
-     "button--confirm": props.confirm,
-     "button--danger": props.danger
-   });
- 
-   return (
-     <button
-       className={buttonClass}
-       onClick={props.onClick}
-       disabled={props.disabled}
-     >
-       {props.children}
-     </button>
-   );
- }
+  const buttonClass = className("button", {
+    "button--confirm": props.confirm,
+    "button--danger": props.danger
+  });
+
+  return (
+    <button
+      className={buttonClass}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
+      {props.children}
+    </button>
+  );
+}
