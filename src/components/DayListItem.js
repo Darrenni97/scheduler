89949308d7ab2d36fd03ae5732e15b7724ef8e56
Darrenni97 <darrenni_@hotmail.com>
2019-10-28 20:34:09 -0700

    Appointment Views

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -2,25 +2,24 @@ import React from "react";
 
 import "components/DayListItem.scss";
 
-const className = require('classnames');
+const className = require("classnames");
 
 export default function DayListItem(props) {
-  const formatSpots = (spots) => {
+  const formatSpots = spots => {
     if (spots === 0) {
-      return 'no spots remaining';
+      return "no spots remaining";
     } else if (spots === 1) {
-      return '1 spot remaining';
-    } else return `${spots} spots remaining`
-  }
+      return "1 spot remaining";
+    } else return `${spots} spots remaining`;
+  };
 
-  const DayClass = className('day-list__item', {
-    'day-list__item--selected': props.selected,
-    'day-list__item--full': props.spots === 0
-  })
+  const DayClass = className("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  });
 
   return (
-    <li className = {DayClass}
-    onClick={() => props.setDay(props.name)}>
+    <li className={DayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
