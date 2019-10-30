26ef40117d59f06b0e3077c115e8346b458f81f0
Darrenni97 <darrenni_@hotmail.com>
2019-10-30 15:31:42 -0700

    using hook to create form and cancel form

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,20 +3,36 @@ import React from "react";
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
+import Form from "components/Appointment/Form";
 
 import "components/Appointment/styles.scss";
+import useVisualMode from "hooks/useVisualMode";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
     <article className="appointment">
       <Header time={props.time}></Header>
-      {props.interview ? (
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer.name}
-        ></Show>
-      ) : (
-        <Empty></Empty>
+        />
+      )}
+      {mode === CREATE && (
+        <Form
+          interviewers={[]}
+          onCancel={() => back()}
+          onSave={console.log()}
+        />
       )}
     </article>
   );
---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  const transition = (mode, replace = false) => {
+    setMode(mode);
+    setHistory(prev => [...prev, mode]);
+
+    if (replace) {
+      setHistory(history.splice(history.length - 2, 1), mode);
+    }
+  };
+
+  const back = () => {
+    if (history.length > 1) {
+      history.pop();
+    }
+    setMode(history[history.length - 1]);
+  };
+
+  return { mode, transition, back };
+}
